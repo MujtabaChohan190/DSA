dc810a768a55e6b30c0bb9716f6f36086e00c31d
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-10-18 13:31:46 +0500

    Update Stack_list.cpp

--- Stack/Stack_list.cpp
+++ Stack/Stack_list.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 struct Node {
     int data;
     Node* next;
@@ -35,6 +38,11 @@ public:
     }
 
     void display() {
+        if (isEmpty()) {
+            cout << "Stack is Empty\n";
+            return;
+        }
+        cout << "Stack elements (top to bottom): ";
         Node* temp = top;
         while (temp) {
             cout << temp->data << " ";
@@ -43,3 +51,49 @@ public:
         cout << endl;
     }
 };
+
+// ===============================
+//            MAIN
+// ===============================
+int main() {
+    StackList s;
+    int choice, value;
+
+    while (true) {
+        cout << "\n--- STACK MENU ---\n";
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek (Top Element)\n";
+        cout << "4. Display\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to push: ";
+                cin >> value;
+                s.push(value);
+                break;
+
+            case 2:
+                s.pop();
+                break;
+
+            case 3:
+                cout << "Top element is: " << s.peek() << endl;
+                break;
+
+            case 4:
+                s.display();
+                break;
+
+            case 5:
+                cout << "Exiting program...\n";
+                return 0;
+
+            default:
+                cout << "Invalid choice! Try again.\n";
+        }
+    }
+}
