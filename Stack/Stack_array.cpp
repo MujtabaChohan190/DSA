b25835d9c12ccf70be2f584afc23163691f78020
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-10-18 13:06:21 +0500

    Update Stack_array.cpp

--- Stack/Stack_array.cpp
+++ Stack/Stack_array.cpp
@@ -31,8 +31,60 @@ public:
     }
 
     void display() {
+        if (isEmpty()) {
+            cout << "Stack is Empty\n";
+            return;
+        }
+        cout << "Stack elements (top to bottom): ";
         for (int i = top; i >= 0; i--)
             cout << arr[i] << " ";
         cout << endl;
     }
 };
+
+// ===============================
+//            MAIN
+// ===============================
+int main() {
+    StackArray s;
+    int choice, value;
+
+    while (true) {
+        cout << "\n--- STACK MENU ---\n";
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek (Top Element)\n";
+        cout << "4. Display\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to push: ";
+                cin >> value;
+                s.push(value);
+                break;
+
+            case 2:
+                s.pop();
+                break;
+
+            case 3:
+                cout << "Top element is: " << s.peek() << endl;
+                break;
+
+            case 4:
+                s.display();
+                break;
+
+            case 5:
+                cout << "Exiting program...\n";
+                return 0;
+
+            default:
+                cout << "Invalid choice! Try again.\n";
+        }
+    }
+}
+
