fe9f5ed85df67654ec2982ada456104c4b694689
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-10-18 21:05:39 +0500

    Create Circular_queue.cpp

---
+++ Queue/Circular_queue.cpp
@@ -0,0 +1,56 @@
+class CircularQueue {
+    int* arr;
+    int currSize, cap;
+    int f, r;
+
+public:
+    CircularQueue(int size) {
+        cap = size;
+        currSize = 0;
+        arr = new int[cap];
+        f = 0;
+        r = -1;
+    }
+
+    void push(int data) {
+        if (currSize == cap) { //check if queue is full 
+            cout << "CQ is FULL\n";
+            return;
+        }
+
+        r = (r + 1) % cap;
+        arr[r] = data;
+        currSize++;
+    }
+
+    void pop() {
+        if (empty()) { //check if queue is empty
+            cout << "CQ is EMPTY\n";
+            return;
+        }
+
+        f = (f + 1) % cap;
+        currSize--;
+    }
+
+    int front() {
+        if (empty()) {
+            cout << "CQ is EMPTY\n";
+            return -1;
+        }
+        return arr[f];
+    }
+
+    bool empty() {
+        return currSize == 0;
+    }
+};
+
+int main() {
+    CircularQueue cq(3);
+    cq.push(1);
+    cq.push(2);
+    cq.push(3);
+    cq.push(4); 
+    return 0;
+}
