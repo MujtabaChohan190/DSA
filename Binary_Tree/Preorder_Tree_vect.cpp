a046966566196a9ecc1e2c58f288b6c9a48e8c6b
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-10-19 23:54:48 +0500

    Update Preorder_Tree_vect.cpp

--- Binary_Tree/Preorder_Tree_vect.cpp
+++ Binary_Tree/Preorder_Tree_vect.cpp
@@ -30,12 +30,23 @@ Node* buildTree(vector<int> preorder) {
     return root;
 }
 
+void preOrder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
 int main() {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
     Node* root = buildTree(preorder);
-    cout << root->data << endl;
-    cout << root->left->data << endl;
-    cout << root->right->data << endl;
+
+    preOrder(root);
+    cout << endl;
+
     return 0;
 }
