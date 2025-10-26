5c04f03dacda9ad4893a81aa74bab1ec96593aaf
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-10-26 14:17:39 +0500

    Update radix_sort.cpp

--- Sorting/radix_sort.cpp
+++ Sorting/radix_sort.cpp
@@ -13,24 +13,33 @@ void countingSort(int arr[], int n, int exp) {
     int output[n];
     int count[10] = {0};
 
-    for(int i = 0; i < n; i++)
-        count[(arr[i] / exp) % 10]++;
+    // Count digits
+    for(int i = 0; i < n; i++) {
+        int digit = (arr[i] / exp) % 10;   // extract digit
+        count[digit]++;
+    }
 
+    // Convert count into cumulative count
     for(int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
+    // Build output array (from right to left for stability)
     for(int i = n - 1; i >= 0; i--) {
-        output[count[(arr[i]/exp) % 10] - 1] = arr[i];
-        count[(arr[i]/exp) % 10]--;
+        int digit = (arr[i] / exp) % 10;   // extract digit again
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
     }
 
+    // Copy output to original
     for(int i = 0; i < n; i++)
         arr[i] = output[i];
 }
 
 void radixSort(int arr[], int n) {
-    int m = getMax(arr, n);
-    for(int exp = 1; m/exp > 0; exp *= 10)
+    int maxNumber = getMax(arr, n);
+
+    // Apply counting sort for each digit place
+    for(int exp = 1; maxNumber / exp > 0; exp *= 10)
         countingSort(arr, n, exp);
 }
 
