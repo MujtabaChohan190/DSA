08608107341b908627a4f14606c15b6a8df648a2
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-11-02 21:24:56 +0500

    Update Insertnodes.cpp

--- BST/Insertnodes.cpp
+++ BST/Insertnodes.cpp
@@ -47,6 +47,22 @@ void inorder(Node* root) {
     inorder(root->right);
 }
 
+bool search(Node* root, int key) {
+    if (root == NULL) {
+        return false;
+    }
+
+    if (root->data == key) {
+        return true;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
 int main() {
     int arr[] = {3, 2, 1, 5, 6, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -55,6 +71,15 @@ int main() {
 
     inorder(root);
     cout << endl;
+
+    return 0;
+}
+
+int main() {
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
+
+    Node* root = buildBST(arr);
+    cout << search(root, 5) << endl; //1
 
     return 0;
 }
