abae1d7007402507b5b5939b01abc8840eb216fa
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2025-11-04 16:23:45 +0500

    Update merge_sort.cpp

--- Sorting/merge_sort.cpp
+++ Sorting/merge_sort.cpp
@@ -1,68 +1,10 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-
-void merge(vector<int> &arr, int st, int mid, int end) {
-    vector<int> temp;
-    int i = st, j = mid + 1;
-
-    while (i <= mid && j <= end) {
-        if (arr[i] <= arr[j]) {
-            temp.push_back(arr[i]);
-            i++;
-        } else {
-            temp.push_back(arr[j]);
-            j++;
-        }
-    }
-
-    while (i <= mid) {
-        temp.push_back(arr[i]);
-        i++;
-    }
-
-    while (j <= end) {
-        temp.push_back(arr[j]);
-        j++;
-    }
-
-    for (int idx = 0; idx < temp.size(); idx++) {
-        arr[idx + st] = temp[idx];
-    }
-}
-
-void mergeSort(vector<int> &arr, int st, int end) {
-    if (st < end) {
-        int mid = st + (end - st) / 2;
-        mergeSort(arr, st, mid); // left half
-        mergeSort(arr, mid + 1, end); // right half
-        merge(arr, st, mid, end);
-    }
-}
-
-int main() {
-    vector<int> arr = {12, 31, 35, 8, 32, 17};
-
-    mergeSort(arr, 0, arr.size() - 1);
-
-    for (int val : arr) {
-        cout << val << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-
-
--------------------------------------------------------------------------
-
-#include <iostream>
 using namespace std;
 
 void merge(int arr[], int st, int mid, int end) {
     int temp[1000]; // temporary array (size should be >= original array size)
     int i = st, j = mid + 1, k = 0;
-
+//in this step , we are checking the smallest values between two sorted arrays which are backtracked and now need to be combined
     while (i <= mid && j <= end) {
         if (arr[i] <= arr[j]) {
             temp[k++] = arr[i++];
@@ -70,7 +12,7 @@ void merge(int arr[], int st, int mid, int end) {
             temp[k++] = arr[j++];
         }
     }
-
+//there could be a case that j pointer array which is second array gets empty after inserting all its elements to temp , so i could be left so hum bachiwi values ko dalenge
     while (i <= mid) {
         temp[k++] = arr[i++];
     }
@@ -81,16 +23,16 @@ void merge(int arr[], int st, int mid, int end) {
 
     // Copy back to original array
     for (int x = 0; x < k; x++) {
-        arr[st + x] = temp[x];
+        arr[st + x] = temp[x]; //our temp start index is 0 but in origiinal array sotted position starts at index - to ensure we place the combined array correctly in sorted  order in orgiianl array we do this
     }
 }
 
 void mergeSort(int arr[], int st, int end) {
     if (st < end) {
         int mid = st + (end - st) / 2;
-        mergeSort(arr, st, mid);
-        mergeSort(arr, mid + 1, end);
-        merge(arr, st, mid, end);
+        mergeSort(arr, st, mid);//left - divide left 
+        mergeSort(arr, mid + 1, end);//right 
+        merge(arr, st, mid, end);//combine left and right 
     }
 }
 
@@ -100,7 +42,7 @@ int main() {
 
     mergeSort(arr, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {  //getting sorted array
         cout << arr[i] << " ";
     }
 
